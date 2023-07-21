641c598b0b4d5d6fa1abf63a74de6effe169ede3
Nestor Nunes <109300513+netinho-RB@users.noreply.github.com>
2023-07-21 15:54:13 -0300

    modifyies

--- public/script.js
+++ public/script.js
@@ -1,45 +1,28 @@
+// Seu arquivo scripts.js
+const dataDiv = document.getElementById('data');
 const usdToBrlDiv = document.getElementById('usd-to-brl');
-const usdToUsdDiv = document.getElementById('usd-to-usd');
 
 async function getExchangeRate() {
   try {
-    const response = await fetch(
-      'https://api.exchangeratesapi.io/latest?base=USD&symbols=BRL'
-    );
+    const response = await fetch('https://api.exchangeratesapi.io/latest?base=USD&symbols=BRL');
     const data = await response.json();
-    const exchangeRateBRL = data.rates?.BRL || 0;
+    const exchangeRateBRL = data.rates?.BRL;
 
-    const today = new Date().toISOString().split('T')[0];
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    const yesterdayFormatted = yesterday.toISOString().split('T')[0];
-
-    const responseYesterday = await fetch(
-      `https://api.exchangeratesapi.io/${yesterdayFormatted}?base=USD&symbols=BRL`
-    );
-    const dataYesterday = await responseYesterday.json();
-    const exchangeRateYesterdayBRL = dataYesterday.rates?.BRL || 0;
-
-    const dailyVariationBRL = exchangeRateBRL - exchangeRateYesterdayBRL;
-
-    usdToBrlDiv.innerHTML = `
-      <p>USD para BRL: ${exchangeRateBRL.toFixed(2)}</p>
-      <p>Variação Diária em BRL: ${dailyVariationBRL.toFixed(4)}</p>
-    `;
-
-    // Mostrar cotação em dólar
-    const exchangeRateUSD = 1 / exchangeRateBRL;
-    const dailyVariationUSD = 1 / exchangeRateYesterdayBRL - exchangeRateUSD;
-
-    usdToUsdDiv.innerHTML = `
-      <p>USD para USD: ${exchangeRateUSD.toFixed(2)}</p>
-      <p>Variação Diária em USD: ${dailyVariationUSD.toFixed(4)}</p>
-    `;
+    if (exchangeRateBRL) {
+      const today = new Date().toISOString().split('T')[0];
+      dataDiv.textContent = today;
+      usdToBrlDiv.textContent = exchangeRateBRL.toFixed(2);
+    } else {
+      throw new Error('Cotação do dólar não encontrada');
+    }
   } catch (error) {
     console.error('Erro ao obter a cotação do dólar:', error);
-    usdToBrlDiv.innerText = 'Erro ao obter a cotação do dólar';
-    usdToUsdDiv.innerText = 'Erro ao obter a cotação do dólar';
+    dataDiv.textContent = 'Erro ao obter a cotação do dólar';
+    usdToBrlDiv.textContent = 'Erro ao obter a cotação do dólar';
   }
 }
 
 getExchangeRate();
+
+const btnAtualizar = document.getElementById('btnAtualizar');
+btnAtualizar.addEventListener('click', getExchangeRate);
